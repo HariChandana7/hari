b18a1e659c25e9746cb6fe9a59227005e3402503
hari <harichandana2023@gmail.com>
2021-11-27 16:37:06 +0530

    updated

--- Day1/Program4.java
+++ Day1/Program4.java
@@ -1,15 +1,18 @@
-import java.util.Scanner;
+import java.util.*;
 class Program4
 {
- public static void main(String... args )
-  {
-   Scanner sc=new Scanner(System.in);
-   for(int i=0; i<=3; i++)
-   {
-     String  s=sc.next();
-     int n=sc.nextInt();
-    System.out.println("\n"+s+"\t" +n);
+     public static void main(String... args )
+     {
+       String S1 = "Java" ,S2="Python" ,S3="JavaScript" ;
+       int values[]={7,777,77};
+       System.out.printf("%-15s%03d\n",S1,values[0]); 
+       System.out.printf("%-15s%03d\n",S2,values[1]);   
+       System.out.printf("%-15s%03d\n",S3,values[2]); 
     }
- }
 }
+
+ /* Output :
+Java           007
+Python         777
+JavaScript     077  */
 
