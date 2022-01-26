bc9415abfd28d23c99c153dc5441c89de8e7dc8d
hari <harichandana2023@gmail.com>
2022-01-26 10:44:48 +0530

    Day13 commit

---
+++ D13RegEx/RegExpExample.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.regex.*;
+
+public class RegExpExample
+   {
+	public static void main(String... args)
+	{
+	  //final String REGEX_PATTERN ="coo.*per";
+	  Pattern  p = Pattern.compile("coo.*per");
+	 try  {
+	            BufferedReader  br = new BufferedReader(new FileReader("C:\\Users\\91991\\Documents"));
+		String line ;
+		while((line = br.readLine()) != null)  {
+		       Matcher  m = p.matcher(line);
+		        if(m.find())
+			System.out.println("Matched Lines are : \n" +line);
+		  }
+	 }
+	 catch(IOException  e)  {
+	       e.printStackTrace();
+	 }
+                 }
+  }
